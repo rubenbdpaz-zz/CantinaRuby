ecd40d21765b00b6deaeacb26dab3302db557ac3
Ruben <rubenbdpaz@gmail.com>
2014-09-03 11:32:59 -0300

    Initial commit

---
+++ Order.rb
@@ -0,0 +1,10 @@
+require_relative 'Product.rb'
+
+#Array de produtos
+#Código
+#Valor
+#Status
+
+def initialize()
+	
+end
---
+++ Product.rb
@@ -0,0 +1,11 @@
+class Product
+
+	attr_accessor :name, :price
+
+	def initialize(name, price)
+		@name = name
+		@price = price
+	end
+
+		
+end
