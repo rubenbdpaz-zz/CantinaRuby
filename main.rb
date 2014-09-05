e840dc99433e148fc63ad039d39421dbbf19e585
Ruben <rubenbdpaz@gmail.com>
2014-09-05 10:22:46 -0300

    Small improvements in the menu

--- main.rb
+++ main.rb
@@ -29,25 +29,32 @@ loop do
 	puts "3 - Entregas"
 	puts "4 - Sair"
 	
-	main_menu_code = gets.to_i	
+	main_menu_code = gets.to_i
+	system "clear" or system "cls"
 	break if main_menu_code == EXIT
 
 	#Menu Estoque
 	if main_menu_code == STOCK
 		
+		loop do
+			puts "1 - Cadastro de Produtos"
+			puts "2 - Controle de Estoque"
+			puts "4 - Voltar"
+			subMenu_code = gets.to_i
+			system "clear" or system "cls"
+			break if subMenu_code == EXIT
 
-		puts "1 - Cadastro de Produtos"
-		puts "2 - Controle de Estoque"
-		puts "3 - Voltar"
-		subMenu_code = gets.to_i
-		break if main_menu_code == EXIT
+			if subMenu_code+10 == REGISTER_PRODUCT
+				puts "Registro de produtos"
+				puts "Pressione enter ..."
+				gets()
+			end
 
-		if subMenu_code == REGISTER_PRODUCT
-			
-		end
-
-		if subMenu_code == STOCK_CONTROL
-			
+			if subMenu_code+10 == STOCK_CONTROL
+				puts "Controle de Estoque"
+				puts "Pressione enter ..."
+				gets()
+			end
 		end
 
 
@@ -56,42 +63,58 @@ loop do
 	#Menu Vendas
 	if main_menu_code == SALES
 		
-		puts "1 - Fazer Pedido"
-		puts "2 - Alterar Pedido"
-		puts "3 - Cancelar Pedido"
-		puts "4 - Voltar"
-		subMenu_code = gets.to_i
-		break if main_menu_code == EXIT
+		loop do
+			puts "1 - Fazer Pedido"
+			puts "2 - Alterar Pedido"
+			puts "3 - Cancelar Pedido"
+			puts "4 - Voltar"
+			subMenu_code = gets.to_i
+			system "clear" or system "cls"
+			break if subMenu_code == EXIT
 
-		if subMenu_code == NEW_ORDER
-			
-		end
+			if subMenu_code+20 == NEW_ORDER
+				puts "Novo pedido"
+				puts "Pressione enter ..."
+				gets()
+			end
 
-		if subMenu_code == EDIT_ORDER
-			
-		end
+			if subMenu_code+20 == EDIT_ORDER
+				puts "Alterar pedido"
+				puts "Pressione enter ..."
+				gets()
+			end
 
-		if subMenu_code == CANCEL_ORDER
-			
+			if subMenu_code+20 == CANCEL_ORDER
+				puts "Cancelar pedido"
+				puts "Pressione enter ..."
+				gets()
+			end
 		end
 	end
 
 	#Menu Entregas
 	if main_menu_code == DELIVERY
 		
+		loop do
 
-		puts "1 - Listar pedidos não entregues"
-		puts "2 - Entregar pedido"
-		puts "3 - Voltar"
-		subMenu_code = gets.to_i
-		break if main_menu_code == EXIT
+			puts "1 - Listar pedidos não entregues"
+			puts "2 - Entregar pedido"
+			puts "4 - Voltar"
+			subMenu_code = gets.to_i
+			system "clear" or system "cls"
+			break if subMenu_code == EXIT
 
-		if subMenu_code == LIST_ORDERS
-			
-		end
+			if subMenu_code+30 == LIST_ORDERS
+				puts "Listar pedidos"
+				puts "Pressione enter ..."
+				gets()
+			end
 
-		if subMenu_code == DELIVERY_ORDER
-			
+			if subMenu_code+30 == DELIVERY_ORDER
+				puts "Entregar pedido"
+				puts "Pressione enter ..."
+				gets()
+			end
 		end
 	end
 end
