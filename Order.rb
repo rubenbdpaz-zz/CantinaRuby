defb675af8ee35a45c9da260d2a93678594422f9
Ruben <rubenbdpaz@gmail.com>
2014-09-12 16:49:36 -0300

    Add status to an order

--- Order.rb
+++ Order.rb
@@ -10,30 +10,47 @@ require_relative 'Product.rb'
 	#Pedido pronto - Pedido já preparado pela cozinha, mas ainda não entregue ao cliente
 	#Pedido entregue - Pedido entregue ao cliente
 
+	#Constantes
+	REGISTERED = 1
+	ACCEPTED = 2
+	READY = 3
+	DELIVERED = 4
+
+
 	attr_reader :id
 	attr_accessor :value :status :arr_products
 
 
 def initialize()
-	
+	@value = 0
+	@status = REGISTERED
+	@arr_products = []
 end
+
+def add_product(product)
+	if status = REGISTERED
+		@arr_products << product
+	else
+		puts "Não foi possivel alterar o pedido."
+end
+
 
 #Método para listar todos os pedidos existentes
 	def list_orders
 	end
 
 	#Método para listar pedidos com status 'feito' existentes // Melhorar nome dessa função
-	def list_orders_1
+	def list_orders_registered
 	end
 
 	#Método para listar pedidos com status 'aceito' existentes // Melhorar nome dessa função
-	def list_orders_2
+	def list_orders_accepted
 	end
 
 	#Método para listar pedidos com status 'pronto' existentes // Melhorar nome dessa função
-	def list_orders_3
+	def list_orders_ready
 	end
 
 	#Método para listar pedidos com status 'entregue' existentes // Melhorar nome dessa função
-	def list_orders_4
+	def list_orders_delivered
 	end
